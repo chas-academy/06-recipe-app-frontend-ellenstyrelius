b5b7f31fbefe17e444624a8620582465f8542d77
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-07-22 17:37:50 +0200

    Change the way the diet parameters are sent in requests to recipe API, since I couldn't get the earlier format to work in my Laravel backend code.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -16,16 +16,14 @@ export class RecipesComponent implements OnInit {
   dietLabels: any;
 
   constructor(private recipesService: RecipesService, private savedRecipesService: SavedRecipesService) {
-    this.healthLabels = ['vegetarian', 'sugar-conscious', 'alcohol-free', 'peanut-free', 'tree-nut-free'];
-    this.dietLabels = ['low-carb', 'low-fat', 'high-protein'];
+    this.dietLabels = ['vegetarian', 'vegan', 'sugar-conscious', 'alcohol-free', 'peanut-free', 'tree-nut-free', 'low-carb', 'low-fat', 'high-protein'];
   }
 
   handleSearch = (form: NgForm) => {
     const dietSelections = this.handleDietSelections(form);
     const input = form.value.search;
     
-    /////////// OBS kommenterat ut argument nedan:
-    const apiRequest = this.recipesService.recipeSearch(/*input, dietSelections*/);
+    const apiRequest = this.recipesService.recipeSearch(input, dietSelections);
     apiRequest.subscribe(data => {
       this.recipes = data;
     });
@@ -38,6 +36,8 @@ export class RecipesComponent implements OnInit {
         diet.push(key);
       }
     }
+
+    console.log('🐐: RecipesComponent -> handleDietSelections -> diet', diet)
     return diet;
   }
 
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -8,6 +8,8 @@ export class RecipesService {
 
   constructor(private http: HttpClient) { }
 
+  url = 'http://recipes.test/api/recipes';
+
   fetchData = {
     method: 'GET',
     headers: new HttpHeaders({
@@ -17,16 +19,19 @@ export class RecipesService {
     })
   }
 
-  url = 'http://recipes.test/api/recipes';
+  recipeSearch(input: string, dietSelection) {
 
-  recipeSearch(/*input: string, dietSelection*/) {
-    // const diet = dietSelection.join('');
+    const diet = '&diet=' + dietSelection.join('+');
     // const apiRequest = this.http.get<any>(`https://api.edamam.com/search?q=${input}&app_id=${appId}&app_key=${apiKey}&to=40${diet}`);
-
+    const search = input.replace('+', ' ');
+    
+    console.log('🐐: RecipesService -> recipeSearch -> search', search)
+    console.log('🐐: RecipesService -> recipeSearch -> diet', diet)
+    
     //////////
     // SET SOME TIMEOUT ON THE REQUEST OR SOMETHING!!!!!!!!
 
-    const apiRequest = this.http.get<any>(this.url, this.fetchData);
+    const apiRequest = this.http.get<any>(`${this.url}/search?q=${input}${diet}`, this.fetchData);
     return apiRequest;
   }
 
