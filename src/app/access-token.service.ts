58726949eaaab4e607791e82d2f0dda032874df8
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-08-02 18:34:32 +0200

    Create access-token-service that gets the access token from local storage and use it to request the logged in user from the database. The idea was to use it in the route guard services but I couldn't get it to work since the canActivate methods just wouldn't wait for the poor get requests to finish before running, regardless of what I did to prevent it. Rude

---
+++ src/app/access-token.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccessTokenService {
+
+  constructor(private http: HttpClient) {}
+
+  url = 'http://recipes.test/api/auth/user';
+  accessToken = localStorage.getItem('accessToken');
+
+  fetchData = {
+    headers: new HttpHeaders({
+      'Authorization': `Bearer ${this.accessToken}`
+    })
+  };
+
+  getAccessToken() {
+    return localStorage.getItem('accessToken');
+  }
+
+  getUser() {
+    if (this.accessToken) {
+      const user = this.http.get<any>(this.url, this.fetchData);
+      user.subscribe(data => {
+        console.log('🐐: AccessTokenService -> getUser -> data', data)
+        return data;
+      });
+    }
+  }
+}
